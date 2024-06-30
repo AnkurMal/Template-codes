154cedae75a3c79fa5e0149f13c38119b3d8b6fb
Ankur Mallick <108927188+AnkurMal@users.noreply.github.com>
2024-06-30 10:14:59 +0530

    Update main.rs

--- Raylib by ffi (Rust)/src/main.rs
+++ Raylib by ffi (Rust)/src/main.rs
@@ -3,8 +3,8 @@ use raylib::*;
 
 fn main() {
     unsafe {
-        assign_cstring!(title, "sample");
-        assign_cstring!(text, "Hello, World");
+        let title = cstring!("sample {}", 1);
+        let text = cstring!("Hello, World");
 
         SetTraceLogLevel(TraceLogLevel::LOG_NONE);
         InitWindow(800, 450, title.as_ptr());
